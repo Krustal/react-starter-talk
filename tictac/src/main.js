869d0c0822d1e8594816b5cf581bae04ea5b9170
Byron McMullen <byronsm@gmail.com>
2016-05-18 11:02:59 -0700

    toggle square

--- tictac/src/main.js
+++ tictac/src/main.js
@@ -1,3 +1,14 @@
-var { a, b, c } = { a: 'hello', b: 'worlds', c: 'ymolo' };
+import React from 'react';
+import ReactDom from 'react-dom';
+import gameStore from './gameStore.js';
+import Square from './square.jsx';
 
-console.log(a, b, c);
+let update = function(x, y) {
+  gameStore.move(x, y);
+  render();
+}
+
+let render = function(){
+  ReactDom.render(<Square player={gameStore.board[0][0]} x={0} y={0} onClick={update}/>, document.getElementById('app'));
+};
+render();
